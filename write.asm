6fce6982b68d6a35553dee0c7873eae79d39148b
Olga Vyrostko <vyrostkoolga@gmail.com>
2016-04-17 11:42:32 +0500

    add: резидент

--- write.asm
+++ write.asm
@@ -5,99 +5,30 @@ org 100h
 
 VIDEO_START	EQU		0B800h
 MAX_NUM			EQU		"9"
-ESCAPE_CODE EQU 1Bh
 
-start   proc    near
-		; Считываем длину командной строки
-		mov   si, 80h
-		lodsb
-		xor   ah, ah
-		mov   cx, ax
+start:
+		jmp main
 
-		; Считываем слово командной строки
-		mov   si, 82h
-		lodsw
-		mov   bx, ax
-		xor   ax, ax
+		interrupt_num   db    0
+		old_timer_handler dd  ?
+		old_handler 			dd  ?
+		old_key_handler 	dd  ?
+		old_pres_handler  dd 	?
+		increment					db	1
+		counter			db		"0", 01Fh, "0", 01Fh, "0", 01Fh, "0", 01Fh, "0", 01Fh
+		et_counter	db		"0", 01Fh, "0", 01Fh, "0", 01Fh, "0", 01Fh, "0", 01Fh
+		cl_counter	db		" ", 00Fh, " ", 00Fh, " ", 00Fh, " ", 00Fh, " ", 00Fh
+		mes					db		"Already in use", 0dh, 0ah, "$"
 
-		; Если длина командной строки < 3 ( односимвольная запись числа )
-		cmp   cx, 3
-		jl    b1
+presence_handler	proc	far
+		cmp ah, 80h
+		je finish_presense
+		jmp dword ptr cs:[old_pres_handler]
 
-		sub   bl, 30h
-		mov   al, 10
-		mul   bl
-		mov    bl, bh
-
-b1:
-		xor    bh, bh
-		sub    bl, 30h
-		add    ax, bx
-
-		mov   [interrupt_num], al
-
-		; Получаем адрес обработчика прерывания системного таймера
-		mov			ax, 351Ch
-    int     21h
-    mov     word ptr old_timer_handler, bx
-    mov     word ptr old_timer_handler+2, es
-
-    ; Устанавливаем наш обработчик прерывания системного таймера
-    mov     ax, 251Ch
-    mov     dx, offset timer_handler
-    int     21h
-
-		; Устанавливаем обработчик клавиатурного прерывания
-    mov     ax, 3509h
-    int     21h
-    mov     word ptr old_key_handler, bx
-    mov     word ptr old_key_handler+2, es
-
-    mov     ax, 2509h
-    mov     dx, offset key_handler
-    int     21h
-
-		; Получаем адрес обработчика нужного нам прерывания
-		mov			ah, 35h
-		mov			al, [interrupt_num]
-    int     21h
-    mov     word ptr old_handler, bx
-    mov     word ptr old_handler+2, es
-
-    ; Устанавливаем наш обработчик
-		mov			ah, 25h
-		mov			al, [interrupt_num]
-    mov     dx, offset int_handler
-    int     21h
-
-    ; check pressed key
-;read_key:
-    ;mov     ah, 07h          ; 07h = got symbol from console without echo
-    ;int     21h
-
-    ;cmp     al, ESCAPE_CODE
-    ;je      quit
-		;jmp			read_key
-
-;quit:
-    ; before exit, reset handlers
-    ;mov     ax, 251Ch
-    ;mov     dx, word ptr old_timer_handler + 2
-    ;mov     ds, dx
-    ;mov     dx, word ptr cs:old_timer_handler
-    ;int     21h
-
-    ret
-
-interrupt_num   db    0
-old_timer_handler dd  ?
-old_handler 			dd  ?
-old_key_handler 	dd  ?
-increment					db	1
-counter			db		"0", 01Fh, "0", 01Fh, "0", 01Fh, "0", 01Fh, "0", 01Fh
-et_counter	db		"0", 01Fh, "0", 01Fh, "0", 01Fh, "0", 01Fh, "0", 01Fh
-cl_counter	db		" ", 00Fh, " ", 00Fh, " ", 00Fh, " ", 00Fh, " ", 00Fh
-start   endp
+finish_presense:
+		mov al, 0ffh
+		ret
+presence_handler endp
 
 int_handler			proc		far
 		; save all registers
@@ -166,8 +97,8 @@ key_handler  proc    far
     mov     dx, 0040h
     mov     ds, dx
 
-    mov     al,byte ptr ds:0017h ; считать байт состояния клавиатуры,
-    test    al,04h                      ; если не нажат Ctrl,
+    mov     al,byte ptr ds:0017h 				; считать байт состояния клавиатуры,
+    test    al,04h                      ; если не нажат Ctrl, не наша комбинация
     jz      not_our_key
 
     mov     dx, cs
@@ -220,6 +151,12 @@ handle_quit:
     jmp  not_our_key
 
 not_our_key:
+		; Здесь нужно убрать символ перед передачей управления дальше
+		;mov     dx, 0040h
+		;mov     ds, dx
+		;mov			di, word ptr ds:001Ah
+		;mov			word ptr ds:001Ch,di
+
     pop     ds
     pop     es
     popa
@@ -227,5 +164,111 @@ not_our_key:
     ret
 
 key_handler endp
+
+main:
+		xor 	ax, ax
+		xor 	dx, dx
+		xor 	bx, bx
+		xor 	cx, cx
+
+		mov 	ah, 80h
+		int 	2fh
+
+		cmp 	al, 0ffh
+		jne		run
+
+stop:
+		mov		ah, 09h
+		mov		dx, offset mes
+		int		21h
+		int		20h
+
+run:
+		xor		ax, ax
+		; Считываем длину командной строки
+		mov   si, 80h
+		lodsb
+		xor   ah, ah
+		mov   cx, ax
+
+		; Считываем слово командной строки
+		mov   si, 82h
+		lodsw
+		mov   bx, ax
+		xor   ax, ax
+
+		; Если длина командной строки < 3 ( односимвольная запись числа )
+		cmp   cx, 3
+		jl    b1
+
+		sub   bl, 30h
+		mov   al, 10
+		mul   bl
+		mov    bl, bh
+
+b1:
+		xor    bh, bh
+		sub    bl, 30h
+		add    ax, bx
+
+		mov   [interrupt_num], al
+
+		; Получаем адрес обработчика прерывания системного таймера
+		cli
+		mov			ax, 351Ch
+    int     21h
+    mov     word ptr old_timer_handler, bx
+    mov     word ptr old_timer_handler+2, es
+
+    ; Устанавливаем наш обработчик прерывания системного таймера
+    mov     ax, 251Ch
+    mov     dx, offset timer_handler
+    int     21h
+		;sti
+
+		; Устанавливаем обработчик клавиатурного прерывания
+		;cli
+    mov     ax, 3509h
+    int     21h
+    mov     word ptr old_key_handler, bx
+    mov     word ptr old_key_handler+2, es
+
+    mov     ax, 2509h
+    mov     dx, offset key_handler
+    int     21h
+		;sti
+
+		; Устанавливаем обработчик мультиплексного прерывания
+		;cli
+		mov			ah, 35h
+		mov			al, 02Fh
+    int     21h
+    mov     word ptr old_pres_handler, bx
+    mov     word ptr old_pres_handler+2, es
+
+    ; Устанавливаем наш обработчик
+		mov			ah, 25h
+		mov			al, 02Fh
+    mov     dx, offset presence_handler
+    int     21h
+		;sti
+
+		; Получаем адрес обработчика нужного нам прерывания
+		;cli
+		mov			ah, 35h
+		mov			al, [interrupt_num]
+    int     21h
+    mov     word ptr old_handler, bx
+    mov     word ptr old_handler+2, es
+
+    ; Устанавливаем наш обработчик
+		mov			ah, 25h
+		mov			al, [interrupt_num]
+    mov     dx, offset int_handler
+    int     21h
+		sti
+
+		mov			dx, offset main
+		int			27h
 
 end start
